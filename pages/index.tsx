65d203048abde7d5a8ffe4dcfa22b9fd4d058716
Jean-Pierre Bergamin <james.bergamin@comparis.ch>
2024-08-06 07:30:53 +0000

    Add zipcode and city

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,15 @@ type Listing = {
   realestateType: "Flat" | "House";
   price: number;
   rooms: number;
+  zipcode: number;
+  city: string;
 };
 
 export const getServerSideProps = (async (context) => {
   const data = await fetch(
     "https://cb7872bd-6f31-48e8-b9dd-4e63f88d2c2e-00-2itmukcy99ju.worf.replit.dev:5000/listing",
   );
+
   const response = (await data.json()) as Listing[];
 
   return {
@@ -42,40 +45,21 @@ const Home = ({
         <h1 className={styles.title}>Welcome to comparis.ch</h1>
         <h2>Here are our rent properties</h2>
         <div className={styles.grid}>
-          {listings
-            .filter((listing) => listing.dealType === "Rent")
-            .map((listing, index) => (
-              <div className={styles.card}>
-                <h2>{listing.title}</h2>
-                <dl className={styles.details}>
-                  <dt>Price:</dt>
-                  <dd>{listing.price}</dd>
-                  <dt>Rooms:</dt>
-                  <dd>{listing.rooms}</dd>
-                  <dt>Type:</dt>
-                  <dd>{listing.realestateType}</dd>
-                </dl>
-              </div>
-            ))}
-        </div>
-
-        <h2>Here are our buy properties</h2>
-        <div className={styles.grid}>
-          {listings
-            .filter((listing) => listing.dealType === "Buy")
-            .map((listing, index) => (
-              <div className={styles.card}>
-                <h2>{listing.title}</h2>
-                <dl className={styles.details}>
-                  <dt>Price:</dt>
-                  <dd>{listing.price}</dd>
-                  <dt>Rooms:</dt>
-                  <dd>{listing.rooms}</dd>
-                  <dt>Type:</dt>
-                  <dd>{listing.realestateType}</dd>
-                </dl>
-              </div>
-            ))}
+          {listings.map((listing, index) => (
+            <div className={styles.card}>
+              <h2>{listing.title}</h2>
+              <dl className={styles.details}>
+                <dt>Price:</dt>
+                <dd>{listing.price}</dd>
+                <dt>Rooms:</dt>
+                <dd>{listing.rooms}</dd>
+                <dt>Type:</dt>
+                <dd>{listing.realestateType}</dd>
+                <dt>City:</dt>
+                <dd>{listing.zipcode} {listing.city}</dd>
+              </dl>
+            </div>
+          ))}
         </div>
       </main>
     </div>
